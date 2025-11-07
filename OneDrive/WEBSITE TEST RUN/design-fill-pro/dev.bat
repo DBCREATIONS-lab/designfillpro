8e6fabd071c8a6d6c63cf09395d98d6dabb5d9bc
Design Fill Pro <coolbilletcustomrifles@gmail.com>
2025-11-07 13:16:24 -0600

    Delete OneDrive/WEBSITE TEST RUN/design-fill-pro directory

--- OneDrive/WEBSITE TEST RUN/design-fill-pro/dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-REM Launches the dev PowerShell helper, forwarding all arguments
-REM Works even when PowerShell script execution is restricted
-
-set SCRIPT_DIR=%~dp0
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%SCRIPT_DIR%dev.ps1' %*"
